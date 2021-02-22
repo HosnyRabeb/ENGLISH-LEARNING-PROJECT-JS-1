24ca59a42fd06ffcd84f6257f89b7f6e1c5ec555
HosnyRabeb <hbebar10@gmail.com>
2021-02-22 16:49:06 +0100

    Changes-Monday

---
+++ js/admin.js
@@ -0,0 +1,12 @@
+function addCourse() {
+    var storage = JSON.parse(localStorage.getItem("STUDENTS")) || [];
+    storage.push({
+        FirstName: FirstName,
+        LastName: LastName,
+        Email: Email,
+        Password: Password,
+        role: "admin"
+    });
+    localStorage.setItem("STUDENTS", JSON.stringify(storage));
+    window.location.href = "course.html";
+}
--- js/login.js
+++ js/login.js
@@ -1,24 +1,23 @@
 function signIn() {
-    var Email = document.getElementById("recipient-name");
-    var Password = document.getElementById("recipient-name1");
-    var storage = JSON.parse(localStorage.getItem("STUDENTS"));
-    console.log(storage);
-        var connectedStudent = JSON.parse(localStorage.getItem("UserConnected")) || [];
+  var Email = document.getElementById("recipient-name");
+  var Password = document.getElementById("recipient-name1");
+  var storage = JSON.parse(localStorage.getItem("STUDENTS"));
+  console.log(storage);
+  var connectedStudent = JSON.parse(localStorage.getItem("UserConnected")) || [];
 
-     connectedStudent = storage.find(
-      (x) => x.Email === Email.value && x.Password=== Password.value
-    );
-  
-    if (connectedStudent === undefined) {
-      alert("VERIFY YOUR EMAIL OR/AND PASSWORD");
-    } else {
-        localStorage.setItem("UserConnected",JSON.stringify(connectedStudent))
-      window.location.href ="course.html";
-    }
-  
-   
-    
+  connectedStudent = storage.find(
+    (x) => x.Email === Email.value && x.Password === Password.value
+  );
 
+  if (connectedStudent === undefined) {
+    alert("VERIFY YOUR EMAIL OR/AND PASSWORD");
+  } else if(connectedStudent.role=="admin"){
+    localStorage.setItem("UserConnected", JSON.stringify(connectedStudent))
+    window.location.href = "admin.html";
+  }
+  else{
+    localStorage.setItem("UserConnected", JSON.stringify(connectedStudent))
+    window.location.href = "course.html";
+  }
 }
 
- 
