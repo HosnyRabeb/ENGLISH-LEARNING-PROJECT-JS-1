1c2cbf46bbe2f71c40f409ff6a1cbba71652420d
HosnyRabeb <hbebar10@gmail.com>
2021-02-22 11:38:59 +0100

    register-storage

---
+++ js/register.js
@@ -0,0 +1,49 @@
+function logUp() {
+    var FirstName = document.getElementById("recipient-name2").value;
+    var LastName = document.getElementById("recipient-name3").value;
+    var Email = document.getElementById("recipient-name4").value;
+    var Password = document.getElementById("recipient-name6").value;
+    
+    // if (FirstName .value == "") {
+    //     FirstName.className += " is-invalid";
+    //   return;
+    // } else {
+    //     FirstName.classList.remove("is-invalid");
+    //   FirstName.className += " is-valid";
+    // }
+    // if (LastName.value == "") {
+    //     LastName.className += " is-invalid";
+    //   return;
+    // } else {
+    //     LastName.classList.remove("is-invalid");
+    //   LastName.className += " is-valid";
+    // }
+    // if (Email.value.indexOf("@") == -1) {
+    //     Email.className += " is-invalid";
+    //   return;
+    // } else {
+    //     Email.classList.remove("is-invalid");
+    //     Email.className += " is-valid";
+    // }
+    // if (Password.value.length < 6) {
+    //     Password.classList.remove("is-valid");
+    //     Password.className += " is-invalid";
+    //   return;
+    // } else {
+    //     Password.classList.remove("is-invalid");
+    //     Password.className += " is-valid";
+    // }
+    
+    var storage = JSON.parse(localStorage.getItem("STUDENTS")) || [];
+  
+    storage.push({
+      FirstName :FirstName,
+      LastName:LastName,
+      Email:Email,
+      Password:Password
+      });
+      localStorage.setItem("STUDENTS", JSON.stringify(storage));
+      window.location.href ="index.html";
+    }
+        
+
